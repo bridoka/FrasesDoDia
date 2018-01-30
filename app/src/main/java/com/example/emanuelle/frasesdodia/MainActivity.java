9cad33898404d4cc5bc3b0aea881a52ede668929
Emanuelle Rizzuto <bridoka@gmail.com>
2018-01-30 20:33:02 -0200

    initial commit

--- app/src/main/java/com/example/emanuelle/frasesdodia/MainActivity.java
+++ app/src/main/java/com/example/emanuelle/frasesdodia/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.emanuelle.frasesdodia;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void gerarNovaFrase(View view){
+        String[] frases = {"Frase 1","Frase 2", "Frase 3", "Frase 4"};
+        int numeroFrase = new Random().nextInt(frases.length);
+        String frase = frases[numeroFrase];
+        TextView texto = findViewById(R.id.txtNovaFrase);
+        texto.setText(frase);
     }
 }
